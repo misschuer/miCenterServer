7474b4b59427fa7c1c87b9d361f672ffd51abdae
gong <403575226@qq.com>
2018-07-02 20:54:42 +0800

    中心服进行消息的接受和处理

--- src/main/java/cc/mi/center/task/DealClientDataTask.java
+++ src/main/java/cc/mi/center/task/DealClientDataTask.java
@@ -3,15 +3,20 @@ package cc.mi.center.task;
 import cc.mi.center.server.CenterServerManager;
 import cc.mi.core.packet.Packet;
 import cc.mi.core.task.base.AbstractCoderTask;
+import io.netty.channel.Channel;
 
 public class DealClientDataTask extends AbstractCoderTask {
-	public DealClientDataTask(Packet coder) {
+	
+	private final Channel channel;
+	public DealClientDataTask(Channel channel, Packet coder) {
 		super(coder);
+		this.channel = channel;
 	}
 	
 	@Override
 	protected void doTask() {
-//		CenterSystemManager.sendMsgToInner(coder);
+		CenterServerManager.INSTANCE.invokeHandler(channel, coder);
+		CenterServerManager.INSTANCE.sendToInnerServerWhenRegisted(coder);
 	}
 
 }
