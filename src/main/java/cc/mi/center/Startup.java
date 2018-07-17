56cc98c582c0a6d8205ac5fcf8091c5a406d7306
gong <403575226@qq.com>
2018-07-17 19:48:54 +0800

    修改启动逻辑

--- src/main/java/cc/mi/center/Startup.java
+++ src/main/java/cc/mi/center/Startup.java
@@ -16,14 +16,18 @@ public class Startup {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				try {
-					while (true) {
+				while (true) {
+					try {
 						ClientCore.INSTANCE.start(ServerConfig.getGateIp(), ServerConfig.getGatePort(), new CenterToGateHandler());
+					} catch (Exception e) {
+					} finally {
 						logger.devLog("连接网关服错误,系统将在1秒钟后重新连接");
-						Thread.sleep(1000);
+						try {
+							Thread.sleep(1000);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
 					}
-				} catch (Exception e) {
-					e.printStackTrace();
 				}
 			}
 		}, "center-to-gate").start();
@@ -31,14 +35,18 @@ public class Startup {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				try {
-					while (true) {
+				while (true) {
+					try {
 						ServerCore.INSTANCE.run(ServerConfig.getCenterPort(), new CenterHandler(), IdentityConst.SERVER_TYPE_CENTER);
+					} catch (Exception e) {
+					} finally {
 						logger.devLog("监听内部服务器端口发生错误,系统将在1秒钟后重新执行");
-						Thread.sleep(1000);
+						try {
+							Thread.sleep(1000);
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
 					}
-				} catch (Exception e) {
-					e.printStackTrace();
 				}
 			}
 		}, "center-for-inner").start();
